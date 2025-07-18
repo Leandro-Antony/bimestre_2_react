c4d08f8d2e133c04daecd3c00e596f9ad88fed7f
Leandro Antony Batista Lemos <20221074010005@ifrn.local>
2025-07-18 17:44:40 -0300

    commit

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,5 @@
 import styles from "./Cart.module.css";
+import { ArrowBigRightDash } from "lucide-react";
 
 export default function Cart({ cart }) {
   return (
@@ -6,20 +7,31 @@ export default function Cart({ cart }) {
     <h2>Shopping Cart</h2>
     <div className={styles.container}>
       {cart.length === 0 ? (
-        <p>Your cart is empty.</p>
+        <p className={styles.empty}>Seu setup tá pedindo um upgrade!</p>
       ) : (
+        <>
+        <div className={styles.track}>
+          <h3> Carrinho <ArrowBigRightDash /> Endereço <ArrowBigRightDash />Entrega <ArrowBigRightDash /> Pagamento <ArrowBigRightDash /> Confirmação <ArrowBigRightDash /> Concluir</h3>
+        </div>
+
         <ul className={styles.cart}>
           {cart.map((product, index) => (
             <li key={index}>
               <div className={styles.product}>
-              <img src={product.thumbnail} alt={product.title} />
+              <img className={styles.image} src={product.thumbnail} alt={product.title} />
               <h3>{product.title}</h3>
               <p>${product.price.toFixed(2)}</p>
+              </div>
+              <div>
+                <h3>Quantidade</h3>
+                <div className={styles.quantity}>
+                </div>
               </div>
             </li>
           ))
           }
         </ul>
+        </>
       )}
     </div>
     </>
