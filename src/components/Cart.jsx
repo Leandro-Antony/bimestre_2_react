c35a67003ae7708cbab4049c86feb68b3d38acb9
Leandro Antony Batista Lemos <20221074010005@ifrn.local>
2025-07-18 14:33:21 -0300

    commit

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,22 +1,27 @@
 import styles from "./Cart.module.css";
 
-export function Cart({ cart }) {
+export default function Cart({ cart }) {
   return (
-    <div className={styles.cart}>
-      <h2>Shopping Cart</h2>
+    <>
+    <h2>Shopping Cart</h2>
+    <div className={styles.container}>
       {cart.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
-        <ul>
+        <ul className={styles.cart}>
           {cart.map((product, index) => (
             <li key={index}>
+              <div className={styles.product}>
               <img src={product.thumbnail} alt={product.title} />
               <h3>{product.title}</h3>
               <p>${product.price.toFixed(2)}</p>
+              </div>
             </li>
-          ))}
+          ))
+          }
         </ul>
       )}
     </div>
+    </>
   );
 }
